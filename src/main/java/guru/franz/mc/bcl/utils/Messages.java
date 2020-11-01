be5a611c98dbf14d234ba69ce98f688b4ae27992
Kasper Franz <kasper.franz@lenstore.co.uk>
2020-11-01 10:11:55 +0000

    final

--- src/main/java/guru/franz/mc/bcl/utils/Messages.java
+++ src/main/java/guru/franz/mc/bcl/utils/Messages.java
@@ -1,16 +1,16 @@
 package guru.franz.mc.bcl.utils;
 
-public class Messages {
-    public static String CREATE_CHUNKLOADER_LOG = "%s created a %s at %s with radius %s";
-    public static String CREATE_CHUNKLOADER_USER = "Chunk loader created to cover %s chunks.";
-    public static String EDIT_CHUNKLOADER_LOG_OTHER = "%s edited %s's chunk loader at %s's radius from %s to %s,";
-    public static String EDIT_CHUNKLOADER_LOG_SELF = "%s edited their chunk loader at %s's radius from %s to %s.";
-    public static String EDIT_CHUNKLOADER_USER = "The chunk loader is now updated to cover %s chunks.";
-    public static String DELETE_CHUNKLOADER_LOG_OTHER = "%s deleted %s's chunk loader at %s.";
-    public static String DELETE_CHUNKLOADER_LOG_SELF = "%s deleted their chunk loader at %s.";
-    public static String DELETE_CHUNKLOADER_USER_OTHER = "You just removed %s's chunk loader at %s.";
-    public static String DELETE_CHUNKLOADER_USER_INFORM = "%s just removed your chunk loader at %s.";
-    public static String DELETE_CHUNKLOADER_USER_SELF = "You just removed your chunk loader at %s.";
+public final class Messages {
+    public static final String CREATE_CHUNKLOADER_LOG = "%s created a %s at %s with radius %s";
+    public static final String CREATE_CHUNKLOADER_USER = "Chunk loader created to cover %s chunks.";
+    public static final String EDIT_CHUNKLOADER_LOG_OTHER = "%s edited %s's chunk loader at %s's radius from %s to %s,";
+    public static final String EDIT_CHUNKLOADER_LOG_SELF = "%s edited their chunk loader at %s's radius from %s to %s.";
+    public static final String EDIT_CHUNKLOADER_USER = "The chunk loader is now updated to cover %s chunks.";
+    public static final String DELETE_CHUNKLOADER_LOG_OTHER = "%s deleted %s's chunk loader at %s.";
+    public static final String DELETE_CHUNKLOADER_LOG_SELF = "%s deleted their chunk loader at %s.";
+    public static final String DELETE_CHUNKLOADER_USER_OTHER = "You just removed %s's chunk loader at %s.";
+    public static final String DELETE_CHUNKLOADER_USER_INFORM = "%s just removed your chunk loader at %s.";
+    public static final String DELETE_CHUNKLOADER_USER_SELF = "You just removed your chunk loader at %s.";
     public static final String LIST_CHUNKLOADERS_TITLE = "%s chunk loaders";
     public static final String LIST_NO_CHUNKLOADERS = "%s has no chunk loaders";
     public static final String LIST_PERMISSION_ERROR = "You do not have permission to delete this chunk loader";
